c918c1f1ab93ae5913ce893a75c7fa81994db603
Ashok Kumar <Ashok.kumar@Pratian.com>
2020-04-02 13:10:04 +0530

    Signed-off-by: Ashok Kumar <Ashok.kumar@Pratian.com>
    Adding patient list and add

--- webapp/patients/urls.py
+++ webapp/patients/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 app_name = 'patients'
 urlpatterns = [
-    path('', views.index, name='index')
-   
+    path('', views.patient_list, name='patient_list'),
+	
+	path('patients', views.patient_list, name='patient_list'),
+	path('patient_delete/<int:pk>', views.patient_delete, name='patient_delete'),
+	path('patient_update/<int:id>', views.patient_update, name='patient_update'),
+	path('patient_create.html', views.patient_create, name='patient_create'),
+	#path('patient', views.patient_detail, name='patient_detail'),
+    #path('patient/<int:pk>', views.PatientDetail.as_view(), name='patient_detail'),
+    #path('create', views.PatientCreate.as_view(), name='patient_create'),
+    #path('update/<int:pk>', views.PatientUpdate.as_view(), name='patient_update'),
+
 ]
