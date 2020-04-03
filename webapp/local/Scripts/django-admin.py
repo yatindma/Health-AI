fa9b08243c4467961d966c82e7abd95e673166a6
Yatin Arora <yatindma11@gmail.com>
2020-04-03 13:12:19 +0530

    Fixed error
    Fixed some issue, cause while copy pasting the code

---
+++ webapp/local/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\yatin.arora\Documents\GitHub\Health_predictor_using_AI\webapp\local\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
