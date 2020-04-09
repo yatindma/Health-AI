bb5fc0b5318fd5876dd5dfd39862f9127d5b331d
Ashok Kumar <Ashok.kumar@Pratian.com>
2020-04-09 14:00:35 +0530

    Added patient update

--- webapp/patients/urls.py
+++ webapp/patients/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 	path('patient_delete/<int:pk>', views.patient_delete, name='patient_delete'),
 	path('patient_create.html', views.patient_create, name='patient_create'),
 	path('patient_upload.html', views.patient_upload, name='patient_upload'),
+	path('patient_update/<int:pk>', views.patient_update, name='patient_update'),
 ]
